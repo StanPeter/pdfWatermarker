0deaae2930d9d709a16533d01bd4c88f178dc901
StanPeter <stanislav.petr7@gmail.com>
2020-12-22 00:56:54 +0100

    readme improved

--- readme.txt
+++ readme.txt
@@ -1,13 +1,26 @@
-A programme to merge all pdf files stored in put_your_pdf_files and put watermark on each page of them
+Information:
 
-Useful when having more PDF separated files(for example book chapters) or just putting watermark on pdf files
 
-0. download the project including all files
-1. create an environment inside the project folder and activate 
-2. install libraries using "pip install -r requirements" on terminal
-3. launch "python watermark.py" and see the final result
-4. now replace all files in "put_your_pdf_files" and "put_your_watermark" by your own :) 
-5. launch python file again 
+    Created to merge all pdf files stored in 'put_your_pdf_files' and put watermark on each page of them
 
-PS: project is super small so I didn't test much errors with "try:" blocks but it serves 
-anyway more like a backup for me 
+    Useful when having more PDF separated files(for example book chapters) or just putting watermark on pdf files
+
+    Using Python3
+
+
+
+Instructions: (commands for Windows might differ)
+
+
+    1. Create a new environment                                 "  python -m venv venv  " 
+
+    2. Activate it                                              "  source venv/bin/activate  "
+
+    3. install required libraries by executing                  "  pip install -r requirements.txt  "
+    in terminal
+
+    4. launch python watermark.py through terminal 
+
+    5. now replace all files in "put_your_pdf_files" and "put_your_watermark" by your own :) 
+
+    6. execute watermark.py yet again 
--- watermark.py
+++ watermark.py
@@ -1,7 +1,10 @@
 import PyPDF2
-import sys, os, time
+import sys
+import os
+import time
 
-#merges all files found in input_file(accepted as argument)
+
+# merges all files found in input_file(accepted as argument)
 def combine_pdf_files():
     input_file = "put_your_pdf_files"
     files = os.listdir(input_file)
@@ -9,30 +12,33 @@ def combine_pdf_files():
 
     for pdf in files:
         merger.append(input_file + "/" + pdf)
-    merger.write("combinedPdf.pdf") #creates new combined file
+    merger.write("combinedPdf.pdf")  # creates new combined file
     print("created combinedPdf.pdf")
     time.sleep(2)
 
-    watermark("combinedPdf.pdf") #launch watermark func
+    watermark("combinedPdf.pdf")  # launch watermark func
 
-#add watermark on each
+
+# add watermark on each
 def watermark(input_file):
     input_pdf = PyPDF2.PdfFileReader(open(input_file, "rb"))
     watermark_file = os.listdir("put_your_watermark")[0]
-    watermark_pdf = PyPDF2.PdfFileReader(open("put_your_watermark/"+ watermark_file, "rb"))
+    watermark_pdf = PyPDF2.PdfFileReader(
+        open("put_your_watermark/" + watermark_file, "rb"))
     output = PyPDF2.PdfFileWriter()
 
-    for i in range(input_pdf.getNumPages()):    #put watermark on each page
+    for i in range(input_pdf.getNumPages()):  # put watermark on each page
         page = input_pdf.getPage(i)
         page.mergePage(watermark_pdf.getPage(0))
         output.addPage(page)
 
-        with open("watermarked.pdf", "wb") as file:   #final output file
+        with open("watermarked.pdf", "wb") as file:  # final output file
             output.write(file)
 
-    os.remove("combinedPdf.pdf")    #finally remove unecessary document
+    os.remove("combinedPdf.pdf")  # finally remove unecessary document
     print("removed previous file")
     print("your watermarked.pdf was created!")
+
 
 if __name__ == "__main__":
     combine_pdf_files()
